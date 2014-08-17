4e100b5513a0a09f7a66be30ee187fc081b31776
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2014-08-17 18:18:12 +0900

    .env file is not found apparently...

--- lib/db/connection.rb
+++ lib/db/connection.rb
@@ -1,2 +1,3 @@
+$LOAD_PATH << File.dirname(__FILE__)
 require 'lib/environment'
 DB = Sequel.connect(ENV['DATABASE_URL'])
--- lib/environment.rb
+++ lib/environment.rb
@@ -2,12 +2,14 @@ require 'uri'
 require 'sequel'
 require 'dotenv'
 
-if File.exists?('.env')
-  Dotenv.load
-elsif File.exists?('../.env')
-  Dotenv.load('../.env')  # Migrations run at '$APP_ROOT/lib' level
-else
-  raise "Could not load running environment: #{ENV.inspect}"
+begin
+  if File.exists?('.env')
+    Dotenv.load
+  elsif File.exists?('../.env')
+    Dotenv.load('../.env')  # Migrations run at '$APP_ROOT/lib' level
+  end
+rescue
+  # skip
 end
 
 DATABASE_URI = URI(ENV['DATABASE_URL'])
